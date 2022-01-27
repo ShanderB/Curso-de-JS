8273d71b06bec6ef0f434e3b2f136d25637ac862
ShanderB <s.hander8910@hotmail.com>
2022-01-27 10:38:52 -0300

    contador

--- Flutter/ola_mundo/lib/main.dart
+++ Flutter/ola_mundo/lib/main.dart
@@ -1,47 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 void main() {
-  runApp(AppWidget(title: 'Test'));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  AppWidget({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.amber),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: GestureDetector(
-        child: Text("Contador: $counter"),
-        onTap: () {
-          setState(() {
-            counter++;
-          });
-        },
-      )),
-      color: Colors.black,
-    );
-  }
+  runApp(AppWidget());
 }
