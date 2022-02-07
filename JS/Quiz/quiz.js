1d6874a2f169b178c6e12d2a4420be58ec62d735
ShanderB <s.hander8910@hotmail.com>
2022-02-07 16:53:03 -0300

    change

--- JS/Quiz/quiz.js
+++ JS/Quiz/quiz.js
@@ -1,20 +1,60 @@
 const axios = require('axios').default
+const readline = require('readline')
+const auth = require('./auth')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 async function getQuiz() {
-    await axios.get('https://quizapi.io/api/v1/questions', { params: { apiKey: 'jyrxGoHnlaOis0yMqUD6qsE9Cj2cOl4V5oHzVHe4', limit: 1 } })
+    //https://quizapi.io/docs/1.0/parameters
+
+    let respondeExternal
+
+    await axios.get('https://quizapi.io/api/v1/questions', { params: { apiKey: auth.apiKey, limit: 1 } })
         .then((res) => {
-            console.log(res.data)
+            respondeExternal = res.data
         })
         .catch((err) => {
-            console.log(err)
+            throw err;
         })
+    return respondeExternal
+}
+
+async function printQuestions() {
+
+    let result = await getQuiz()
+    let letter = ['a', 'b', 'c', 'd', 'e', 'f']
+    let counter = 0;
+
+    console.log('======================================================================================================')
+    console.log(result[0].question)
+    console.log('======================================================================================================\n')
+
+    for (answer in result[0].answers) {
+        let isPrintQuestion = result[0].answers[answer]
+
+        isPrintQuestion ? console.log(`${letter[counter]} - ${isPrintQuestion}`) : ''
+        counter++
+    }
+    console.log('\n======================================================================================================\n\n')
+
+    console.log(result[0].correct_answer)
+
+    rl.question('', function (answer) {
+        if (result[0].correct_answer.substring(7, 8) === answer) {
+            console.log('Correct!')
+            result[0].explanation ? console.log(`\n${result[0].explanation}`) : console.log('')
+        } else {
+            console.log('Wrong!')
+        }
+
+        rl.close();
+    })
+
 
 }
 
-async function chec() {
-    console.log('\n\n\n\n\n\n\n')
-    await getQuiz()
-    console.log('\n\n\n\n\n\n\n\n')
 
-}
-chec()
+printQuestions()
