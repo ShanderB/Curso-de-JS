ddab32e32ae2a17b70f563bb9953bd3cd6906aca
Shander Andrade <s.hander8910@hotmail.com>
2022-03-05 20:19:08 -0300

    refac

--- ReactNative/AwesomeProject/src/screens/home.js
+++ ReactNative/AwesomeProject/src/screens/home.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 import Icon from '@expo/vector-icons/MaterialCommunityIcons'
-import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { View, Text, Image, ImageBackground, TextInput } from 'react-native'
 
 export default class Home extends React.Component {
@@ -11,20 +11,20 @@ export default class Home extends React.Component {
                 style={{ width: '100%', height: '100%' }}>
                 <View
                     style={{
-                        flexDirection: "row",
+                        flexDirection: 'row',
                         marginTop: 40,
-                        alignItems: "center",
+                        alignItems: 'center',
                         paddingHorizontal: 40
                     }}>
-                    <Icon name="menu" size={30} color="#a2a2db" style={{ width: 20 }}></Icon>
-                    <Icon name="account-circle" size={33} color="#a2a2db" style={{ marginLeft: 230 }}></Icon>
+                    <Icon name='menu' size={30} color='#a2a2db' style={{ width: 20 }}></Icon>
+                    <Icon name='account-circle' size={33} color='#a2a2db' style={{ marginLeft: 230 }}></Icon>
                 </View>
 
                 <View style={{ paddingHorizontal: 40, marginTop: 25 }}>
                     <Text style={{
                         fontSize: 40,
-                        color: "#522289",
-                        fontFamily: "RobotoBold"
+                        color: '#522289',
+                        fontFamily: 'RobotoBold'
                     }}>
                         Hello
                     </Text>
@@ -34,29 +34,29 @@ export default class Home extends React.Component {
                         paddingVertical: 10,
                         paddingRight: 80,
                         lineHeight: 22,
-                        fontFamily: "RobotoRegular",
-                        color: "#a2a2db"
+                        fontFamily: 'RobotoRegular',
+                        color: '#a2a2db'
                     }}>
                         Lorem ipsum dolor sit amet, consectetuer adipscing elit.
                     </Text>
 
                     <View
                         style={{
-                            flexDirection: "row",
-                            backgroundColor: "#FFF",
+                            flexDirection: 'row',
+                            backgroundColor: '#FFF',
                             borderRadius: 40,
-                            alignItems: "center",
+                            alignItems: 'center',
                             paddingVertical: 10,
                             paddingHorizontal: 20,
                             marginTop: 30,
                         }}>
                         <Image source={require('../images/search.jpg')} style={{ width: 14, height: 14 }} />
                         <TextInput
-                            placeholder="Loremzin"
+                            placeholder='Loremzin'
                             style={{
                                 paddingHorizontal: 20,
                                 fontSize: 15,
-                                color: "#ccccef"
+                                color: '#ccccef'
                             }}
                         />
                     </View>
@@ -66,68 +66,69 @@ export default class Home extends React.Component {
                         showsHorizontalScrollIndicator={false}
                         style={{ marginRight: -40, marginTop: 30 }}
                     >
-                        <View
+                        <TouchableOpacity
+                        onPress={()=>this.props.navigation.navigate('Detail')}
                             style={{
-                                alignItems: "center",
-                                justifyContent: "center",
+                                alignItems: 'center',
+                                justifyContent: 'center',
                                 height: 66,
                                 width: 66,
                                 borderRadius: 50,
-                                backgroundColor: "white"
+                                backgroundColor: 'white'
                             }}
                         >
                             <Image
                                 source={require('../images/p.jpg')}
                                 style={{ height: 24, width: 24 }}
                             />
-                        </View>
+                        </TouchableOpacity>
 
-                        <View
+                        <TouchableOpacity
                             style={{
-                                alignItems: "center",
-                                justifyContent: "center",
+                                alignItems: 'center',
+                                justifyContent: 'center',
                                 height: 66,
                                 width: 66,
                                 borderRadius: 50,
-                                backgroundColor: "#5facdb",
+                                backgroundColor: '#5facdb',
                                 marginLeft: 22
                             }}
                         >
-                            <Icon name="bus" color="white" size={32} />
-                        </View>
+                            <Icon name='bus' color='white' size={32} />
+                        </TouchableOpacity>
 
-                        <View
+                        <TouchableOpacity
                             style={{
-                                alignItems: "center",
-                                justifyContent: "center",
+                                alignItems: 'center',
+                                justifyContent: 'center',
                                 height: 66,
                                 width: 66,
                                 borderRadius: 50,
-                                backgroundColor: "#bb32fe",
+                                backgroundColor: '#bb32fe',
                                 marginLeft: 22
                             }}
                         >
-                            <Icon name="dots-horizontal" color="white" size={32} />
-                        </View>
+                            <Icon name='dots-horizontal' color='white' size={32} />
+                        </TouchableOpacity>
 
-                        <View
+                        <TouchableOpacity
                             style={{
-                                alignItems: "center",
-                                justifyContent: "center",
+                                alignItems: 'center',
+                                justifyContent: 'center',
                                 height: 66,
                                 width: 66,
                                 borderRadius: 50,
-                                backgroundColor: "#bb32fe",
+                                backgroundColor: '#bb32fe',
                                 marginLeft: 22
                             }}
                         >
-                            <Icon name="office-building" color="white" size={32} />
-                        </View>
+                            <Icon name='office-building' color='white' size={32} />
+                        </TouchableOpacity>
                     </ScrollView>
 
                     <Text style={{
-                        color: "#FFF",
-                        fontFamily: "RobotoRegular",
+                        color: '#FFF',
+                        fontFamily: 'RobotoRegular',
                         marginTop: 50,
                         fontSize: 17
                     }}>
@@ -140,7 +141,7 @@ export default class Home extends React.Component {
                         style={{ marginHorizontal: -40, marginTop: 30 }}
                     >
                         <View style={{
-                            backgroundColor: "#FEFEFE",
+                            backgroundColor: '#FEFEFE',
                             height: 200,
                             width: 190,
                             borderRadius: 15,
@@ -154,25 +155,25 @@ export default class Home extends React.Component {
                                     height: 130
                                 }} />
                             <View style={{
-                                flexDirection: "row",
+                                flexDirection: 'row',
                                 width: 150,
-                                alignItems: "center",
+                                alignItems: 'center',
                                 paddingHorizontal: 5,
                                 paddingVertical: 5,
                             }}>
                                 <Text style={{
-                                    fontFamily: "RobotoRegular",
+                                    fontFamily: 'RobotoRegular',
                                     fontSize: 11,
-                                    color: "#a2a2db"
+                                    color: '#a2a2db'
                                 }}>
                                     Lorem ipsum dolor sit amet, consectetuer adipscing elit.
                                 </Text>
-                                <Icon name="map-marker" size={25} color="#ff5c83" />
+                                <Icon name='map-marker' size={25} color='#ff5c83' />
                             </View>
                         </View>
 
                         <View style={{
-                            backgroundColor: "#FEFEFE",
+                            backgroundColor: '#FEFEFE',
                             height: 200,
                             width: 190,
                             borderRadius: 15,
@@ -186,25 +187,25 @@ export default class Home extends React.Component {
                                     height: 130
                                 }} />
                             <View style={{
-                                flexDirection: "row",
+                                flexDirection: 'row',
                                 width: 150,
-                                alignItems: "center",
+                                alignItems: 'center',
                                 paddingHorizontal: 5,
                                 paddingVertical: 5,
                             }}>
                                 <Text style={{
-                                    fontFamily: "RobotoRegular",
+                                    fontFamily: 'RobotoRegular',
                                     fontSize: 11,
-                                    color: "#a2a2db"
+                                    color: '#a2a2db'
                                 }}>
                                     Lorem ipsum dolor sit amet, consectetuer adipscing elit.
                                 </Text>
-                                <Icon name="map-marker" size={25} color="#ff5c83" />
+                                <Icon name='map-marker' size={25} color='#ff5c83' />
                             </View>
                         </View>
 
                         <View style={{
-                            backgroundColor: "#FEFEFE",
+                            backgroundColor: '#FEFEFE',
                             height: 200,
                             width: 190,
                             borderRadius: 15,
@@ -218,20 +219,20 @@ export default class Home extends React.Component {
                                     height: 130
                                 }} />
                             <View style={{
-                                flexDirection: "row",
+                                flexDirection: 'row',
                                 width: 150,
-                                alignItems: "center",
+                                alignItems: 'center',
                                 paddingHorizontal: 5,
                                 paddingVertical: 5,
                             }}>
                                 <Text style={{
-                                    fontFamily: "RobotoRegular",
+                                    fontFamily: 'RobotoRegular',
                                     fontSize: 11,
-                                    color: "#a2a2db"
+                                    color: '#a2a2db'
                                 }}>
                                     Lorem ipsum dolor sit amet, consectetuer adipscing elit.
                                 </Text>
-                                <Icon name="map-marker" size={25} color="#ff5c83" />
+                                <Icon name='map-marker' size={25} color='#ff5c83' />
                             </View>
                         </View>
 
