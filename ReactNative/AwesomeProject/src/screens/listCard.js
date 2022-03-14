646fa9ee760c1e9649cee950ada9cc9c4913a92d
Shander Andrade <s.hander8910@hotmail.com>
2022-03-14 10:51:18 -0300

    refact

--- ReactNative/AwesomeProject/src/screens/listCard.js
+++ ReactNative/AwesomeProject/src/screens/listCard.js
@@ -4,93 +4,93 @@ import { View, Text, TouchableOpacity } from 'react-native'
 export default class ListCard extends React.Component {
     render() {
         return (
-           <TouchableOpacity
-           onPress={this.props.onPress}
-           style={{
-               paddingHorizontal:36,
-               alignSelf:'center',
-               marginTop:20,
-               backgroundColor:'#FFF',
-               height:182,
-               elevation:1,
-               width:270,
-               borderRadius:15
-           }}>
-               <View style={{
-                   flexDirection:'row',
-                   paddingTop:20,
-                   alignSelf:'center'
-               }}>
-                   <Text style={{
-                       fontFamily:'RobotoBold',
-                       color:'#4b3ca7',
-                       fontSize:20
-                   }}>
-                       NYC
-                   </Text>
+            <TouchableOpacity
+                onPress={this.props.onPress}
+                style={{
+                    paddingHorizontal: 36,
+                    alignSelf: 'center',
+                    marginTop: 20,
+                    backgroundColor: '#FFF',
+                    height: 182,
+                    elevation: 1,
+                    width: 270,
+                    borderRadius: 15
+                }}>
+                <View style={{
+                    flexDirection: 'row',
+                    paddingTop: 20,
+                    alignSelf: 'center'
+                }}>
+                    <Text style={{
+                        fontFamily: 'RobotoBold',
+                        color: '#4b3ca7',
+                        fontSize: 20
+                    }}>
+                        NYC
+                    </Text>
 
-                   <Text style={{
-                       color:'#4b3ca7',
-                       fontSize:20,
-                       paddingHorizontal:12
-                   }}>
-                       ....................
-                   </Text>
-                   <Text style={{
-                       fontFamily:'RobotoBold',
-                       color:'#4b3ca7',
-                       fontSize:20,
-                       paddingHorizontal:12
-                   }}>
-                       IDN
-                   </Text>
-               </View>
+                    <Text style={{
+                        color: '#4b3ca7',
+                        fontSize: 20,
+                        paddingHorizontal: 12
+                    }}>
+                        ....................
+                    </Text>
+                    <Text style={{
+                        fontFamily: 'RobotoBold',
+                        color: '#4b3ca7',
+                        fontSize: 20,
+                        paddingHorizontal: 12
+                    }}>
+                        IDN
+                    </Text>
+                </View>
 
-               <View style={{
-                   flexDirection:'row',
-                   marginTop:5,
-                   alignItems:'center'
-               }}>
-                   <Text style={{
-                       fontFamily:'RobotoRegualar',
-                       color:'#a2adb',
-                       fontSize:11
-                   }}>
+                <View style={{
+                    flexDirection: 'row',
+                    marginTop: 5,
+                    alignItems: 'center'
+                }}>
+                    <Text style={{
+                        fontFamily: 'RobotoRegualar',
+                        color: '#a2adb',
+                        fontSize: 11
+                    }}>
                         New York
-                   </Text>
-                   <Text style={{
-                       fontFamily:'RobotoRegualar',
-                       color:'#a2adb',
-                       fontSize:11,
-                       paddingHorizontal:134
-                   }}>
+                    </Text>
+                    <Text style={{
+                        fontFamily: 'RobotoRegualar',
+                        color: '#a2adb',
+                        fontSize: 11,
+                        paddingHorizontal: 134
+                    }}>
                         Indonesia
-                   </Text>
-               </View>
+                    </Text>
+                </View>
 
-               <View style={{
-                   flexDirection:'row',
-                   marginTop: 10,
-                   alignItems: 'center'
-               }}>
-                   <Text style={{
-                       fontFamily:'RobotoRegular',
-                       color: '#522289',
-                       fontSize:16
-                   }}>
-                       09:00 AM
-                   </Text>
-                   <Text style={{
-                       fontFamily:'RobotoRegular',
-                       color: '#522289',
-                       fontSize:16,
-                       paddingLeft: 100
-                   }}>
-                       21:00 PM
-                   </Text>
-               </View>
+                <View style={{
+                    flexDirection: 'row',
+                    marginTop: 10,
+                    alignItems: 'center'
+                }}>
+                    <Text style={{
+                        fontFamily: 'RobotoRegular',
+                        color: '#522289',
+                        fontSize: 16
+                    }}>
+                        09:00 AM
+                    </Text>
+                    <Text style={{
+                        fontFamily: 'RobotoRegular',
+                        color: '#522289',
+                        fontSize: 16,
+                        paddingLeft: 100
+                    }}>
+                        21:00 PM
+                    </Text>
+                </View>
 
-           </TouchableOpacity>
+            </TouchableOpacity>
         );
     }
 }
