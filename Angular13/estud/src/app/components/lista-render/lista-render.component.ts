1e487fc0a1785cb06ac2679f0a721bf12bc476d8
Shander Andrade <s.hander8910@hotmail.com>
2022-08-29 16:42:40 -0300

    interface + ngIf + ngFor

--- Angular13/estud/src/app/components/lista-render/lista-render.component.ts
+++ Angular13/estud/src/app/components/lista-render/lista-render.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/interfaces/Animal';
 @Component({
   selector: 'app-lista-render',
   templateUrl: './lista-render.component.html',
   styleUrls: ['./lista-render.component.less']
 })
 export class ListaRenderComponent implements OnInit {
-  animais = [
+  animais: Animal[] = [
     {nome: "Macaco", tipo: "sla"},
     {nome: "Ajax", tipo: "asd"},
     {nome: "Luzas", tipo: "11111"},
     {nome: "Podsa", tipo: "456"},
   ]
-  constructor() { }
+  showType!: boolean;
 
+  constructor() { }
+  mostrarTipo(): void {
+    this.showType = !this.showType;
+  }
   ngOnInit(): void {
   }
 
