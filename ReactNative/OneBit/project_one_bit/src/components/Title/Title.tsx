7e1630619ca7bc0556563ba91594db1cfd640558
Shander Andrade <s.hander8910@hotmail.com>
2022-09-22 14:39:21 -0300

    {SHANDER}[Title] title style

--- ReactNative/OneBit/project_one_bit/src/components/Title/Title.tsx
+++ ReactNative/OneBit/project_one_bit/src/components/Title/Title.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./styleTitle";
 
 export default function Title(){
     return(
-        <View>
-            <Text>Health</Text>
+        <View style={styles.title}>
+            <Text style={styles.textTitle}>Health</Text>
         </View>
     )
 }
