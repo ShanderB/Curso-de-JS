a68a5941c8e39d20c32b336716eea6ba4c785be3
Shander Andrade <s.hander8910@hotmail.com>
2022-10-02 16:32:28 -0300

    init

--- ReactNative/OneBit/OneBitCoin/App.tsx
+++ ReactNative/OneBit/OneBitCoin/App.tsx
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Title from './src/components/Title/Title';
 import { StyleSheet, Text, View } from 'react-native';
-import Main from './src/components/Main/Main';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Title></Title>
-      <Main></Main>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -16,7 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e0e5e5',
-    paddingTop: 80
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
